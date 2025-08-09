343d2e91ac3ad0d2fbd232a37cbbd9d21b8a6d63
Andras Kovacs <ka841001@gmail.com>
2025-08-09 19:21:41 +0200

    remove pointless sessions

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,16 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# --- remove Steam/Gamescope sessions ---
+dnf5 -y remove gamescope-session gamescope-session-steam || true
+
+# --- delete leftover “one-shot” session files (not owned by any pkg) ---
+rm -f \
+  /usr/share/wayland-sessions/plasma-steamos-wayland-oneshot.desktop \
+  /usr/share/xsessions/plasma-steamos-oneshot.desktop || true
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
